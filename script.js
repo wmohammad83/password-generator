223c0849b2d635f18f19548122eefa9ec2d93d1e
Waseem Mohammad <wmohammad83@icloud.com>
2023-01-06 20:21:42 +0000

    passwordcriteria, randomnumbers created to keep track of input, getpassword function populated to create criteria

--- script.js
+++ script.js
@@ -88,20 +88,69 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+var passwordCriteria = {
+  characters: 0,
+  uppercase: false,
+  lowercase: false,
+  numeric: false,
+  specialCharacters: false
+}
+
+var randomNumbers = {
+  characterType: [],
+  uppercase: [],
+  lowercase: [],
+  numeric: [],
+  specialCharacters:[]
+}
+
 // Function to prompt user for password options
 function getPasswordOptions() {
+// Length of password
+passwordCriteria.characters = prompt('Please choose a number of characters between 10 and 64')
+  
+// Lowercase
+passwordCriteria.lowercase = confirm('Would you like lowercase letters?')
+if(passwordCriteria.lowercase){
+  randomNumbers.characterType.push(true)
+}
 
+// Uppercase
+passwordCriteria.uppercase = confirm('Would you like uppercase letters?')
+if(passwordCriteria.uppercase){
+  randomNumbers.characterType.push(true)
+}
+
+// Numeric
+passwordCriteria.numeric = confirm('Would you like uppercase letters?')
+if(passwordCriteria.numeric){
+  randomNumbers.characterType.push(true)
+}
+// Special characters ($@%&*, etc)
+passwordCriteria.specialCharacters = confirm('Would you like special characters?')
+if(passwordCriteria.specialCharacters){
+  randomNumbers.characterType.push(true)
+}
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  var num = Math.floor(Math.random() * arr.length)
+  // currently getting a .5 number which may cause issues
+  return num
 }
 
 // Function to generate password with user input
 function generatePassword() {
-
+  getPasswordOptions()
+ 
+  console.log(passwordCriteria.characters / randomNumbers.characterType.length)
 }
+
+
+// =================================================================================================
+// Don't touch below here
+// =================================================================================================
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
